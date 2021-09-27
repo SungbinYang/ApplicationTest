33604cbbfe50403f2b108c83c3d9516cfb4ee50b
Robert.Yang <ysb05222@naver.com>
2021-09-28 05:38:41 +0900

    JUnit5 테스트 이름 표시하기
    JUnit5 테스트 이름 표시하기

--- java-test/src/test/java/me/sungbin/javatest/StudyTest.java
+++ java-test/src/test/java/me/sungbin/javatest/StudyTest.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class StudyTest {
 
     @Test
-    void create() {
+    @DisplayName("스터디 만들기 \uD83D\uDE1E")
+    void create_new_study() {
         Study study = new Study();
         assertNotNull(study);
         System.out.println("create");
@@ -15,7 +17,8 @@ class StudyTest {
 
     @Test
     @Disabled
-    void create1() {
+    @DisplayName("스터디 만들기 \uD83E\uDD24")
+    void create_new_study_again() {
         System.out.println("create1");
     }
 
