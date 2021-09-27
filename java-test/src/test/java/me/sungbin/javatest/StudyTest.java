22ff08c575d9c20ec333cc3538dc115f2fbc0d08
Robert.Yang <ysb05222@naver.com>
2021-09-28 05:26:30 +0900

    JUnit5 시작하기
    JUnit5 시작하기

---
+++ java-test/src/test/java/me/sungbin/javatest/StudyTest.java
@@ -0,0 +1,41 @@
+package me.sungbin.javatest;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudyTest {
+
+    @Test
+    void create() {
+        Study study = new Study();
+        assertNotNull(study);
+        System.out.println("create");
+    }
+
+    @Test
+    @Disabled
+    void create1() {
+        System.out.println("create1");
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("before All");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("after all");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("before Each");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("After Each");
+    }
+}
