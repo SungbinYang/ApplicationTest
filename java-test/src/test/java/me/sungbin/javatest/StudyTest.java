c6f31066f1403bd48d026211c02bb54e8522ad36
Robert.Yang <ysb05222@naver.com>
2021-10-01 05:29:18 +0900

    JUnit 5: 테스트 반복하기
    JUnit 5: 테스트 반복하기

--- java-test/src/test/java/me/sungbin/javatest/StudyTest.java
+++ java-test/src/test/java/me/sungbin/javatest/StudyTest.java
@@ -1,17 +1,24 @@
 package me.sungbin.javatest;
 
 import org.junit.jupiter.api.*;
-import org.junit.jupiter.api.condition.*;
+import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.aggregator.AggregateWith;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregationException;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregator;
+import org.junit.jupiter.params.converter.ArgumentConversionException;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.converter.SimpleArgumentConverter;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.time.Duration;
-import java.util.function.Supplier;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assumptions.assumeTrue;
-import static org.junit.jupiter.api.Assumptions.assumingThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTimeout;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class StudyTest {
@@ -72,11 +79,41 @@ class StudyTest {
         System.out.println("test" + repetitionInfo.getCurrentRepetition() + "/" + repetitionInfo.getTotalRepetitions());
     }
 
+//    //    @ValueSource(ints = {10, 20, 40})
+//    //    @NullAndEmptySource
+//    @DisplayName("파라미터 반복 테스트")
+//    @ParameterizedTest(name = "{index} {displayName} message={0}")
+//    @CsvSource({"10, '자바 스터디'", "20, 스프링"})
+//    void parameterizedTest(ArgumentsAccessor argumentsAccessor) {
+//        Study study = new Study(argumentsAccessor.getInteger(0), argumentsAccessor.getString(1));
+//        System.out.println(study);
+//    }
+
+    //    @ValueSource(ints = {10, 20, 40})
+    //    @NullAndEmptySource
     @DisplayName("파라미터 반복 테스트")
     @ParameterizedTest(name = "{index} {displayName} message={0}")
-    @ValueSource(strings = {"날씨가", "많이", "추워지고", "있네요."})
-    void parameterizedTest(String message) {
-        System.out.println(message);
+    @CsvSource({"10, '자바 스터디'", "20, 스프링"})
+    void parameterizedTest(@AggregateWith(StudyAggregator.class) Study study) {
+        System.out.println(study);
+    }
+
+    static class StudyAggregator implements ArgumentsAggregator {
+
+        @Override
+        public Object aggregateArguments(ArgumentsAccessor argumentsAccessor, ParameterContext parameterContext) throws ArgumentsAggregationException {
+            return new Study(argumentsAccessor.getInteger(0), argumentsAccessor.getString(1));
+        }
+    }
+
+    // ValueSource의 매개뱐수의 타입을 메소드 파라미터에 있는 타입으로 변환
+    static class StudyConverter extends SimpleArgumentConverter {
+
+        @Override
+        protected Object convert(Object o, Class<?> aClass) throws ArgumentConversionException {
+            assertEquals(Study.class, aClass, "Can only convert to Study");
+            return new Study(Integer.parseInt(o.toString()));
+        }
     }
 
     @BeforeAll
