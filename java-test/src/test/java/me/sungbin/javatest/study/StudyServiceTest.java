8a62b9d6e16ad66e29334327d47cad0bfe8bcd17
Robert.Yang <ysb05222@naver.com>
2021-10-07 05:12:39 +0900

    Mock 객체 Stubbing
    Mock 객체 Stubbing

--- java-test/src/test/java/me/sungbin/javatest/study/StudyServiceTest.java
+++ java-test/src/test/java/me/sungbin/javatest/study/StudyServiceTest.java
@@ -1,5 +1,7 @@
 package me.sungbin.javatest.study;
 
+import me.sungbin.javatest.domain.Member;
+import me.sungbin.javatest.domain.Study;
 import me.sungbin.javatest.member.MemberService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -7,15 +9,53 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class StudyServiceTest {
 
     @Test
-    void createStudyService(@Mock MemberService memberService, @Mock StudyRepository studyRepository) {
-        StudyService studyService = new StudyService(memberService, studyRepository);
+    void createNewStudy(@Mock MemberService memberService, @Mock StudyRepository studyRepository) {
 
+        StudyService studyService = new StudyService(memberService, studyRepository);
         assertNotNull(studyService);
+
+        Member member = new Member();
+        member.setId(1L);
+        member.setEmail("sungbin@email.com");
+
+//        when(memberService.findById(any())).thenReturn(Optional.of(member));
+
+//        assertEquals("sungbin@email.com", memberService.findById(1L).get().getEmail());
+//        assertEquals("sungbin@email.com", memberService.findById(2L).get().getEmail());
+//
+//        doThrow(new IllegalArgumentException()).when(memberService).validate(1L);
+//
+//        assertThrows(IllegalArgumentException.class, () -> {
+//            memberService.validate(1L);
+//        });
+//
+//        memberService.validate(2L);
+
+        when(memberService.findById(any()))
+                .thenReturn(Optional.of(member))
+                .thenThrow(new RuntimeException())
+                .thenReturn(Optional.empty());
+
+        Optional<Member> byId = memberService.findById(1L);
+        assertEquals("sungbin@email.com", byId.get().getEmail());
+
+        assertThrows(RuntimeException.class, () -> {
+            memberService.findById(1L);
+        });
+
+        assertEquals(Optional.empty(), memberService.findById(3L));
+
+//        studyService.createNewStudy(1L, study);
     }
 }
